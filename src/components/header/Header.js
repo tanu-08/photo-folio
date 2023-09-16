8a684392fa1c8bd68364b4d9681b6e0c665859a1
HP <tanu.bhardwaj@agicent.in>
2023-09-16 11:24:43 +0530

    Initialize Project and Added Header Of Project

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+export default function Header(){
+    return(
+        <>
+        <div className="header-container">
+            <img className="icon-img" src="picture.png" alt='GalleryImage'/>
+            <h1 className="photo-folio-head">PhotoFolio</h1>
+        </div>
+        </>
+    )
+}
